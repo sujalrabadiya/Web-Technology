896b208d9a65d57321dc7f348d8edd1f13b8524e
Sujal Rabadiya <sujalrabadiya0@gmail.com>
2025-01-21 18:40:27 +0530

    Update user-model.js

--- CRUD Operation/models/user-model.js
+++ CRUD Operation/models/user-model.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     age: { type: Number, required: true },
-    email: { type: String, unique: true, required: true },
+    imgUrl: { type: String, unique: true, required: true },
     createdAt: { type: Date, default: Date.now },
 });
 
