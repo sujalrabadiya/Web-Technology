81923b6f1f40939a516d05f5ce63fa4465b5529a
Sujal Rabadiya <sujalrabadiya0@gmail.com>
2025-02-01 12:37:33 +0530

    Update user-router.js

--- CRUD-Operation/routes/user-router.js
+++ CRUD-Operation/routes/user-router.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { addUser, getAllUsers, getUserById, updateUser, deleteUser } = require('../controllers/user-controller')
+const { addUser, addUsers, getAllUsers, getUserById, updateUser, deleteUser } = require('../controllers/user-controller')
 
 // Create a new user
 router.post('/create', addUser);
+
+// Create a new user
+router.post('/createMany', addUsers);
 
 // Get all users
 router.get('/', getAllUsers);
